badef75855964d674dd631ba82564772514e5515
Chad Bibler <chadkeck@gmail.com>
2011-08-26 16:20:49 -0500

    Initial commit.

---
+++ js/list_of_protocols.js
@@ -0,0 +1,870 @@
+g_protocols = [
+	{
+		url: 'http://en.wikipedia.org/wiki/Ethernet',
+		name: 'Ethernet',
+		description: 'Ethernet',
+		layers: [1, 2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Generic_Framing_Procedure',
+		name: 'GFP',
+		description: 'Generic Framing Procedure',
+		layers: [1, 2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Optical_Transport_Network',
+		name: 'OTN',
+		description: 'Optical Transport Network',
+		layers: [1, 2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/ARCnet',
+		name: 'ARCnet',
+		description: 'Resource Computer NETwork',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/ARP',
+		name: 'ARP',
+		description: 'Address Resolution Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/RARP',
+		name: 'RARP',
+		description: 'Reverse Address Resolution Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Cisco_Discovery_Protocol',
+		name: 'CDP',
+		description: 'Cisco Discovery Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Data Link Switching Client Access Protocol',
+		name: 'DCAP',
+		description: 'Data Link Switching Client Access Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Dynamic_Trunking_Protocol',
+		name: 'DTP',
+		description: 'Dynamic Trunking Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Econet',
+		name: 'Econet',
+		description: 'Econet',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Fiber_distributed_data_interface',
+		name: 'FDDI',
+		description: 'Fiber Distributed Data Interface',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Frame_Relay',
+		name: 'Frame Relay',
+		description: 'Frame Relay',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/ITU-T',
+		name: 'ITU-T G.hn',
+		description: 'ITU-T G.hn Data Link Layer',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/High-Level_Data_Link_Control',
+		name: 'HDLC',
+		description: 'High-Level Data Link Control',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IEEE_802.11',
+		name: 'IEEE 802.11',
+		description: 'WiFi',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IEEE_802.16',
+		name: 'IEEE 802.16',
+		description: 'WiMAX',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/LocalTalk',
+		name: 'LocalTalk',
+		description: 'LocalTalk',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/L2F',
+		name: 'L2F',
+		description: 'Layer 2 Forwarding Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/L2TP',
+		name: 'L2TP',
+		description: 'Layer 2 Tunneling Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Link_Access_Procedures,_D_channel',
+		name: 'LAPD',
+		description: 'Link Access Procedures on the D channel',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Link_Layer_Discovery_Protocol',
+		name: 'LLDP',
+		description: 'Link Layer Discovery Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/LLDP-MED',
+		name: 'LLDP-MED',
+		description: 'Link Layer Discovery Protocol - Media Endpoint Discovery',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Point-to-Point_Protocol',
+		name: 'PPP',
+		description: 'Point-to-Point Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Point-to-point_tunneling_protocol',
+		name: 'PPTP',
+		description: 'Point-to-point tunneling protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Message_Transfer_Part',
+		name: 'Q.710',
+		description: 'Simplified Message Transfer Part',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Neighbor_Discovery_Protocol',
+		name: 'NDP',
+		description: 'Neighbor Discovery Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Resilient_Packet_Ring',
+		name: 'RPR',
+		description: 'IEEE 802.17 Resilient Packet Ring',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Serial_Line_Internet_Protocol',
+		name: 'SLIP',
+		description: 'Serial Line Internet Protocol (obsolete)',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/StarLAN',
+		name: 'StarLAN',
+		description: 'StarLAN',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Spanning_Tree_Protocol',
+		name: 'STP',
+		description: 'Spanning Tree Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Token_ring',
+		name: 'Token ring',
+		description: 'Not a protocol, but a topology',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/VTP',
+		name: 'VTP',
+		description: 'VLAN Trunking Protocol',
+		layers: [2],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Asynchronous_Transfer_Mode',
+		name: 'ATM',
+		description: 'Asynchronous Transfer Mode',
+		layers: [2, 3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Frame_relay',
+		name: 'Frame relay',
+		description: 'A simplified version of X.25 welcome',
+		layers: [2, 3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Multi-protocol_label_switching',
+		name: 'MPLS',
+		description: 'Multi-protocol label switching',
+		layers: [2, 3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/X.25',
+		name: 'X.25',
+		description: 'X.25',
+		layers: [2, 3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Message_Transfer_Part',
+		name: 'MTP',
+		description: 'Message Transfer Part',
+		layers: [1, 2, 3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Signalling_Connection_Control_Part',
+		name: 'NSP',
+		description: 'Signalling Connection Control Part',
+		layers: [1, 2, 3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/CLNP',
+		name: 'CLNP',
+		description: 'Connectionless Networking Protocol',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Exterior_Gateway_Protocol',
+		name: 'EGP',
+		description: 'Exterior Gateway Protocol',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/EIGRP',
+		name: 'EIGRP',
+		description: 'Enhanced Interior Gateway Routing Protocol',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Internet_Control_Message_Protocol',
+		name: 'ICMP',
+		description: 'Internet Control Message Protocol',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IGMP',
+		name: 'IGMP',
+		description: 'Internet Group Management Protocol',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IGRP',
+		name: 'IGRP',
+		description: 'Interior Gateway Routing Protocol',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IPv4',
+		name: 'IPv4',
+		description: 'Internet Protocol version 4',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IPv6',
+		name: 'IPv6',
+		description: 'Internet Protocol version 6',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IPSec',
+		name: 'IPSec',
+		description: 'Internet Protocol Security',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IPX',
+		name: 'IPX',
+		description: 'Internetwork Packet Exchange',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Signalling_Connection_Control_Part',
+		name: 'SCCP',
+		description: 'Signalling Connection Control Part',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/AppleTalk_DDP',
+		name: 'AppleTalk DDP',
+		description: 'AppleTalk DDP',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IS-IS',
+		name: 'IS-IS',
+		description: 'Intermediate System-to-Intermediate System',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Open_Shortest_Path_First',
+		name: 'OSPF',
+		description: 'Open Shortest Path First',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Border_Gateway_Protocol',
+		name: 'BGP',
+		description: 'Border Gateway Protocol',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Routing_Information_Protocol',
+		name: 'RIP',
+		description: 'Routing Information Protocol',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/ICMP_Router_Discovery_Protocol',
+		name: 'ICMP Router Discovery Protocol',
+		description: 'Implementation of RFC 1256',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/w/index.php?title=Gateway_Discovery_Protocol',
+		name: 'GDP',
+		description: 'Gateway Discovery Protocol',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Host_Identity_Protocol',
+		name: 'HIP',
+		description: 'Host Identity Protocol',
+		layers: [3],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/AppleTalk',
+		name: 'AppleTalk',
+		description: 'AppleTalk',
+		layers: [3, 4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/DECnet',
+		name: 'DECnet',
+		description: 'DECnet',
+		layers: [3, 4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IPX/SPX',
+		name: 'IPX/SPX',
+		description: 'IPX/SPX',
+		layers: [3, 4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Internet_Protocol_Suite',
+		name: 'Internet Protocol Suite',
+		description: 'Internet Protocol Suite',
+		layers: [3, 4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Xerox_Network_Systems',
+		name: 'Xerox Network Systems',
+		description: 'Xerox Network Systems',
+		layers: [3, 4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Authentication_Header',
+		name: 'AH',
+		description: 'Authentication Header over IP or IPSec',
+		layers: [4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Encapsulating_Security_Payload',
+		name: 'ESP',
+		description: 'Encapsulating Security Payload over IP or IPSec',
+		layers: [4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Generic_Routing_Encapsulation',
+		name: 'GRE',
+		description: 'Generic Routing Encapsulation for tunneling',
+		layers: [4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IL_Protocol',
+		name: 'IL',
+		description: 'Originally developed as transport layer for 9P',
+		layers: [4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Stream_Control_Transmission_Protocol',
+		name: 'SCTP',
+		description: 'Stream Control Transmission Protocol',
+		layers: [4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Sinec_H1',
+		name: 'Sinec H1',
+		description: 'For telecontrol',
+		layers: [4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/IPX/SPX',
+		name: 'IPX/SPX',
+		description: 'Sequenced Packet Exchange',
+		layers: [4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Transmission_Control_Protocol',
+		name: 'TCP',
+		description: 'Transmission Control Protocol',
+		layers: [4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/User_datagram_protocol',
+		name: 'UDP',
+		description: 'User Datagram Protocol',
+		layers: [4],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/9P',
+		name: '9P',
+		description: 'Distributed file system protocol developed originally as part of Plan 9 from Bell Labs',
+		layers: [5],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/NetWare_Core_Protocol',
+		name: 'NCP',
+		description: 'NetWare Core Protocol',
+		layers: [5],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Network_File_System_(protocol)',
+		name: 'NFS',
+		description: 'Network File System',
+		layers: [5],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Server_message_block',
+		name: 'SMB',
+		description: 'Server Message Block',
+		layers: [5],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/SOCKS',
+		name: 'SOCKS',
+		description: 'SOCKetS',
+		layers: [5],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Advanced_Direct_Connect',
+		name: 'ADC',
+		description: 'Advanced Direct Connect (peer-to-peer file sharing protocol)',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Apple_Filing_Protocol',
+		name: 'AFP',
+		description: 'Apple Filing Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/BACnet',
+		name: 'BACnet',
+		description: 'Building Automation and Control Network protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/BitTorrent_(protocol)',
+		name: 'BitTorrent',
+		description: 'A peer-to-peer file sharing protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/BOOTP',
+		name: 'BOOTP',
+		description: 'Bootstrap Protocol',
+		layers: [7],
+
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/CAMEL',
+		name: 'CAMEL',
+		description: 'An SS7 protocol tool for the home operator',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Diameter_(protocol)',
+		name: 'Diameter',
+		description: 'An authentication, authorization and accounting protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/DICOM',
+		name: 'DICOM',
+		description: 'Includes a network protocol definition',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/DICT',
+		name: 'DICT',
+		description: 'Dictionary protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Domain_Name_System',
+		name: 'DNS',
+		description: 'Domain Name System',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Dynamic_Host_Configuration_Protocol',
+		name: 'DHCP',
+		description: 'Dynamic Host Configuration Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/EDonkey_network',
+		name: 'ED2K',
+		description: 'EDonkey network (peer-to-peer file sharing protocol)',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/File_Transfer_Protocol',
+		name: 'FTP',
+		description: 'File Transfer Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Finger_protocol',
+		name: 'Finger',
+		description: 'Gives user profile information',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Gnutella',
+		name: 'Gnutella',
+		description: 'A peer-to-peer file-swapping protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Gopher_(protocol)',
+		name: 'Gopher',
+		description: 'A hierarchical hyperlinkable protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/HTTP',
+		name: 'HTTP',
+		description: 'Hypertext Transfer Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Internet_Message_Access_Protocol',
+		name: 'IMAP',
+		description: 'Internet Message Access Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Internet_Relay_Chat',
+		name: 'IRC',
+		description: 'Internet Relay Chat',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/ISDN_User_Part',
+		name: 'ISUP',
+		description: 'ISDN User Part',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Extensible_Messaging_and_Presence_Protocol',
+		name: 'XMPP',
+		description: 'Extensible Messaging and Presence Protocol (an instant-messaging protocol)',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Lightweight_Directory_Access_Protocol',
+		name: 'LDAP',
+		description: 'Lightweight Directory Access Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/MIME',
+		name: 'MIME',
+		description: 'Multipurpose Internet Mail Extensions',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Microsoft_Notification_Protocol',
+		name: 'MSNP',
+		description: 'Microsoft Notification Protocol (used by Windows Live Messenger)',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Mobile_Application_Part',
+		name: 'MAP',
+		description: 'Mobile Application Part',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/NetBIOS',
+		name: 'NetBIOS',
+		description: 'File Sharing and Name Resolution protocol - the basis of file sharing with Windows.',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Network_News_Transfer_Protocol',
+		name: 'NNTP',
+		description: 'Network News Transfer Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Network_time_protocol',
+		name: 'NTP',
+		description: 'Network time protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/National_Transportation_Communications_for_Intelligent_Transportation_System_Protocol',
+		name: 'NTCIP',
+		description: 'National Transportation Communications for Intelligent Transportation System Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Post_Office_Protocol',
+		name: 'POP3',
+		description: 'Post Office Protocol Version 3',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/RADIUS',
+		name: 'RADIUS',
+		description: 'An authentication, authorization and accounting protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Rlogin',
+		name: 'Rlogin',
+		description: 'A UNIX remote login protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Rsync',
+		name: 'Rsync',
+		description: 'A file transfer protocol for backups, copying and mirroring',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Real-time_Transport_Protocol',
+		name: 'RTP',
+		description: 'Real-time Transport Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/RTSP',
+		name: 'RTSP',
+		description: 'Real-time Transport Streaming Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Secure_shell',
+		name: 'SSH',
+		description: 'Secure shell',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/SISNAPI',
+		name: 'SISNAPI',
+		description: 'Siebel Internet Session Network API',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Session_Initiation_Protocol',
+		name: 'SIP',
+		description: 'Session Initiation Protocol, a signaling protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Simple_Mail_Transfer_Protocol',
+		name: 'SMTP',
+		description: 'Simple Mail Transfer Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Simple_network_management_protocol',
+		name: 'SNMP',
+		description: 'Simple network management protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Simple_Object_Access_Protocol',
+		name: 'SOAP',
+		description: 'Simple Object Access Protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/STUN',
+		name: 'STUN',
+		description: 'Session Traversal Utilities for NAT',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Telephone_User_Part',
+		name: 'TUP',
+		description: 'Telephone User Part',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Telnet',
+		name: 'Telnet',
+		description: 'A remote terminal access protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Transaction_Capabilities_Application_Part',
+		name: 'TCAP',
+		description: 'Transaction Capabilities Application Part',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/Trivial_File_Transfer_Protocol',
+		name: 'TFTP',
+		description: 'Trivial File Transfer Protocol, a simple file transfer protocol',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/WebDAV',
+		name: 'WebDAV',
+		description: 'Web Distributed Authoring and Versioning',
+		layers: [7],
+	},
+
+	{
+		url: 'http://en.wikipedia.org/wiki/DSM-CC',
+		name: 'DSM-CC',
+		description: 'Digital Storage Media Command and Control',
+		layers: [7],
+	},
+];
